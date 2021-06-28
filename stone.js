c88c2059fe177c97be4a0120a898990be5724362
testnjtest <79029547+testnjtest@users.noreply.github.com>
2021-06-27 20:34:38 -0400

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,26 @@
+class Stone {
+    constructor(x, y,width,height) {
+      var options = {
+        'density':12,
+        'friction': 0.9,
+        'restitution':0.8
+      };
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = width;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill('black');
+      strokeWeight(4);
+      stroke('black');
+      rect(0, 0, this.width, this.height);
+      pop();
+    }; 
+  };
